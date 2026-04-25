bc047100b7f9ba4b1f47451bf1782398eef0ba51
Sneha Khenwal <khenwalsnehas@gmail.com>
2026-04-25 13:24:54 +0530

    Added api-gateway,auth-service,college-service microservices

---
+++ backend/college-service/src/test/java/com/college_service/college_service/CollegeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college_service.college_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
